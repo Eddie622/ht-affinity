5a441816dd5078cf2f7fadc35b9f9a404b76f188
Eddie622 <eddietorres622@gmail.com>
2023-05-17 02:25:52 -0400

    restructure

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
 
 function htaffinity_load_scripts(){
     wp_enqueue_style( 'htaffinity-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css'), 'all' );
+    wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/assets/js/dropdown.js', array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'htaffinity_load_scripts' );
