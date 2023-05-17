eb13938abb70d5d893a89a0731ef8988682ca4b0
Eddie622 <eddietorres622@gmail.com>
2023-05-17 02:01:51 -0400

    enqueue styles & fonts

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+
+function htaffinity_load_scripts(){
+    wp_enqueue_style( 'htaffinity-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css'), 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'htaffinity_load_scripts' );
