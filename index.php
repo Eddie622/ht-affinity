414203168c8bd5cc14084a62b82e13ba395f5234
Eddie622 <eddietorres622@gmail.com>
2023-05-17 01:20:10 -0400

    added basic markup

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HT Affinity</title>
-</head>
-<body>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore consequuntur porro a adipisci et ipsam ducimus dolore consequatur pariatur facere maiores facilis recusandae modi, magnam repudiandae! Recusandae saepe assumenda veniam.</p>
-</body>
-</html>
+<?php get_header(); ?>
+        <div id="content" class="sitecontent">
+            <div id="primary" class="cotent-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <section class="home-blog">
+                        Blog
+                    </section>
+                </main>
+            </div>
+        </div>
+<?php get_footer(); ?>
