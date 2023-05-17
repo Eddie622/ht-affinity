b5afa0a27543e63d7732163ad33ddb9430a57396
Eddie622 <eddietorres622@gmail.com>
2023-05-17 03:23:49 -0400

    menus added

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ function htaffinity_load_scripts(){
     wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/assets/js/dropdown.js', array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'htaffinity_load_scripts' );
+
+register_nav_menus(
+    array(
+        'htaffinity_main_menu' => 'Main Menu',
+        'htaffinity_footer_menu' => 'Footer Menu'
+    )
+);
